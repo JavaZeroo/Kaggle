41e975ffb34158c4f9e930566113b41ccdaaafee
JavaZero <2487163254@qq.com>
2022-09-27 20:23:50 +0800

    update

--- findeffnet-m.py
+++ findeffnet-m.py
@@ -3,15 +3,16 @@ from tensorboardX import SummaryWriter
 from torch.autograd import Variable
 import torchvision as tv
 from torchvision.models.feature_extraction import create_feature_extractor
+from torch import nn
 
-WEIGHTS = tv.models.efficientnet.EfficientNet_V2_M_Weights.DEFAULT
+WEIGHTS = tv.models.efficientnet.EfficientNet_V2_S_Weights.DEFAULT
 
 writer = SummaryWriter(log_dir='./log', comment='effnet')
 
 class EffnetModel(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        effnet = tv.models.efficientnet_v2_m(weights=WEIGHTS)
+        effnet = tv.models.efficientnet_v2_s(weights=WEIGHTS)
         self.model = create_feature_extractor(effnet, ['flatten'])
         with writer:
             writer.add_graph(effnet, (Variable(torch.rand(32, 3, 384, 384)),))
@@ -31,8 +32,28 @@ class EffnetModel(torch.nn.Module):
     def predict(self, x):
         frac, vert = self.forward(x)
         return torch.sigmoid(frac), torch.sigmoid(vert)
+
+class ChannelAttention(nn.Module):
+    def __init__(self, in_planes, ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+
+        self.fc1 = nn.Conv2d(in_planes, in_planes // ratio, 1, bias=False)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Conv2d(in_planes // ratio, in_planes, 1, bias=False)
+
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
+        max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
+        out = avg_out + max_out
+        return self.sigmoid(out)
+
+
 print('model')
 model = EffnetModel()
 print('model')
-with writer:
-    writer.add_graph(model, (Variable(torch.rand(1, 3, 384, 384)),))
+# with writer:
+#     writer.add_graph(model, (Variable(torch.rand(1, 3, 384, 384)),))
